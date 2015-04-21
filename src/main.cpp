fb23b5cd38feaea36515ddfdece851916ea36841
Roman C. Podolski <roman.podolski89@gmail.de>
2015-04-21 11:55:05 +0200

    init project

---
+++ src/main.cpp
@@ -0,0 +1,140 @@
+﻿#define GLFW_INCLUDE_GLU
+#include <GLFW/glfw3.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <Eigen/Dense>
+#include <Eigen/StdVector>
+
+//camera settings
+const int camera_width = 640;
+const int camera_height = 480;
+const int virtual_camera_angle = 60;
+unsigned char bkgnd[camera_width*camera_height * 3];
+
+/* program & OpenGL initialization */
+void initGL(int argc, char *argv[])
+{
+	// initialize the GL library
+	// pixel storage/packing stuff
+	glPixelStorei(GL_PACK_ALIGNMENT, 1); // for glReadPixels​
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // for glTexImage2D​
+	glPixelZoom(1.0, -1.0);
+
+	// enable and set colors
+	glEnable(GL_COLOR_MATERIAL);
+	glClearColor(0, 0, 0, 1.0);
+
+	// enable and set depth parameters
+	glEnable(GL_DEPTH_TEST);
+	glClearDepth(1.0);
+
+	// light parameters
+	GLfloat light_pos[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+	GLfloat light_amb[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+	GLfloat light_dif[] = { 0.9f, 0.9f, 0.9f, 1.0f };
+
+	// enable lighting
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_amb);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_dif);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glShadeModel(GL_SMOOTH);
+}
+
+void initVideoStream(cv::VideoCapture &cap)
+{
+	if (cap.isOpened())
+		cap.release();
+
+	cap.open(0); // open the default camera
+}
+
+void display(GLFWwindow* window, const cv::Mat &img_bgr)
+{
+	memcpy(bkgnd, img_bgr.data, sizeof(bkgnd));
+
+	int width0, height0;
+	glfwGetFramebufferSize(window, &width0, &height0);
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// draw background image
+	glDisable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0.0, camera_width, 0.0, camera_height);
+
+	glRasterPos2i(0, camera_height - 1);
+	glDrawPixels(camera_width, camera_height, GL_BGR_EXT, GL_UNSIGNED_BYTE, bkgnd);
+
+	glPopMatrix();
+
+	glEnable(GL_DEPTH_TEST);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char* argv[])
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	// initialize the window system
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(camera_width, camera_height, "Combine", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	int window_width, window_height;
+	glfwGetFramebufferSize(window, &window_width, &window_height);
+	glViewport(0, 0, window_width, window_height);
+
+	// initialize the GL library
+	initGL(argc, argv);
+
+	// setup OpenCV
+	cv::Mat img_bgr;
+	cv::VideoCapture cap;
+	initVideoStream(cap);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		cap >> img_bgr;
+
+		if (img_bgr.empty()){
+			std::cout << "Could not query frame. Trying to reinitialize." << std::endl;
+			initVideoStream(cap);
+			cv::waitKey(1000); // Wait for one sec.
+			continue;
+		}
+
+		/* Render here */
+		display(window, img_bgr);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
