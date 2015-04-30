da46d6a09e0a902c0e48961dc3d9ff41e58c4c3f
Michael <kosokmichael@gmail.com>
2015-04-30 11:05:52 +0200

    Border detection enabled

--- src/markertracker/markertracker.cpp
+++ src/markertracker/markertracker.cpp
@@ -1,19 +1,34 @@
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include "markertracker.h"
 
 
-
-
-class MarkerTracker {
-	public:		
-		void find(cv::Mat &img_bgr);
-};
-		
+MarkerTracker::MarkerTracker() {
+	
+	thresh = 100;
+	max_value = 255;
+	
+}
 		
 		
 void MarkerTracker::find(cv::Mat &img_bgr) {
-	
-	
-	cv::cvtColor(img_bgr, img_bgr, CV_RGB2GRAY);
+
+  cv::Mat img_grey, img_thresh;
+  cv::vector<cv::vector<cv::Point>> contours0;
+  cv::vector<cv::Vec4i> hierarchy;
+
+  cv::cvtColor(img_bgr, img_grey, CV_RGB2GRAY);
+  cv::blur(img_grey,img_grey, cv::Size(3,3));
+  cv::threshold(img_grey,img_thresh,this->thresh,this->max_value,cv::THRESH_BINARY);
+  cv::findContours(img_thresh,
+		  contours0,
+		  hierarchy,
+		  CV_RETR_TREE,
+		  CV_CHAIN_APPROX_SIMPLE,
+		  cv::Point(0,0));
+  contours.resize(contours0.size());
+  for (size_t i = 0; i < contours0.size(); ++i) {
+	  // Process Polygons here
+	  cv::approxPolyDP(cv::Mat(contours0[i]), contours[i], 3, true); 
+	  cv::drawContours(img_bgr,contours,i,cv::Scalar(0,165,255),2,8,hierarchy, 0, cv::Point());
+  }
 	
 }
--- src/markertracker/markertracker.h
+++ src/markertracker/markertracker.h
@@ -1,13 +1,21 @@
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#ifndef MARKERTRAKER_H
+#define MARKERTRAKER_H
 
+
+#include <opencv/cv.h>
 
 
 
 class MarkerTracker {
 	public:
+		MarkerTracker();
 		void find(cv::Mat &img_bgr);
+	private:
+		double thresh;
+		double max_value;
+		cv::vector<cv::vector<cv::Point>> contours;
 };
 		
 		
 		
+#endif
