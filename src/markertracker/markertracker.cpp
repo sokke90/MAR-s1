cac26b477fe37cb570c6b34127d2d321ed218bfa
Michael <kosokmichael@gmail.com>
2015-04-30 12:26:27 +0200

    we limited polygons to 4 edges and are showing a rectangle bounding box

--- src/markertracker/markertracker.cpp
+++ src/markertracker/markertracker.cpp
@@ -16,6 +16,7 @@ void MarkerTracker::find(cv::Mat &img_bgr) {
   Mat img_grey, img_thresh;
   vector<cv::vector<cv::Point>> contours0;
   vector<cv::Vec4i> hierarchy;
+ 
 
 	// make the image B/W
 	cvtColor(img_bgr, img_grey, CV_RGB2GRAY);
@@ -32,14 +33,24 @@ void MarkerTracker::find(cv::Mat &img_bgr) {
 		  CHAIN_APPROX_SIMPLE,
 		  Point(0,0));
 		  
-		 
+	
 	contours.resize(contours0.size());
+	 
+	// we want bounding rectangles now around our stuff - new var here.
+	vector<Rect> boundRect( contours.size() );
 	
 	
 	for (size_t i = 0; i < contours0.size(); ++i) {
+		
 		// Process Polygons here
-		approxPolyDP(Mat(contours0[i]), contours[i], 4, true); 
-		drawContours(img_bgr,contours,i,Scalar(0,165,255),2,8,hierarchy, 0, cv::Point());
+		approxPolyDP(Mat(contours0[i]), contours[i], 3, true);
+		boundRect[i] = boundingRect( Mat(contours[i]) );
+		
+		if (contours[i].size() == 4) {
+			drawContours(img_bgr,contours,i,Scalar(0,165,255),2,8,hierarchy, 0, cv::Point());
+			rectangle( img_bgr, boundRect[i].tl(), boundRect[i].br(), Scalar(100,165,255), 2, 8, 0 );
+		}
 	}
+	
 	
 }
